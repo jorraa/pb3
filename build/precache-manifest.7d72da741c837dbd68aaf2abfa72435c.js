0e3929f828182d28b00d82553d84f52ddaa597be
Jorma Raatikainen <raatikainen.jorma@gmail.com>
2020-04-11 19:52:41 +0300

    Heroku palauttaa 404, logissa 304, minimimuutos

---
+++ build/precache-manifest.7d72da741c837dbd68aaf2abfa72435c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0228935d5ea9a254c24a4ce804a2eac0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fd108062ef79af833e1",
+    "url": "/static/css/main.096542e1.chunk.css"
+  },
+  {
+    "revision": "5fccbfb30e88660cad5f",
+    "url": "/static/js/2.c96b95df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c96b95df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fd108062ef79af833e1",
+    "url": "/static/js/main.ffaac3c5.chunk.js"
+  },
+  {
+    "revision": "d19032c1263fa01ea41a",
+    "url": "/static/js/runtime-main.08633c47.js"
+  }
+]);
